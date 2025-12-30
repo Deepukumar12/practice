b2fbb6ecee8a6bbaac0b40305120a557c7ba68af
Deepu kumar <deeputhakur0986@gmail.com>
2025-12-30 09:14:45 +0530

    I wrote the about course content inside the  README file

---
+++ FRONTEND/project/quizApplication/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const questions = [
+        {
+            question: "What is the capital of India",
+            choices: ["Delhi", "New Delhi", "Patna", "Lucknow"]
+        }
+    ]
+});
